61f74803f832b1174032f602380fca2172274fb0
mcjop <mcjopas@gmail.com>
2026-03-29 15:49:01 +0200

    29.03.2026 Bartłomiej Jopek 7 pkt

--- src/main/java/pl/wsb/FitnessTracker.java
+++ src/main/java/pl/wsb/FitnessTracker.java
@@ -1,9 +1,15 @@
 package pl.wsb;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Component;
+import pl.wsb.fitnesstracker.statistics.api.StatisticsRepository;
+import pl.wsb.fitnesstracker.training.internal.TrainingRepository;
 
 @EntityScan(basePackages = "pl.wsb.fitnesstracker")
 @EnableJpaRepositories(basePackages = "pl.wsb.fitnesstracker")
